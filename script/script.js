e1759f9ac99c1c0f5a11f15b5c0a688a9b5141ed
rladmsrl2 <96101705+rladmsrl2@users.noreply.github.com>
2021-12-23 16:35:44 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('.icon_top').fadeIn();
+    } else {
+        $('.icon_top').fadeOut();
+    }
+});
+
+
